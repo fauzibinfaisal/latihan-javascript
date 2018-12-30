7ad0249b74dd0276943f4416cc739c30efcec07d
fauzi <fauzibinfaisal@gmail.com>
2018-12-30 21:13:59 +0700

    video 28 latihan inline js external js variabel dan console

---
+++ script.js
@@ -0,0 +1,26 @@
+//console.log("Hello");
+
+document.getElementById('heading').innerHTML = "Hello fauzi!";
+
+//Variable
+
+var name = "Fauzi";
+console.log(name);
+
+var lastName = "doank";
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullName = name + ' ' +lastName;
+console.log(fullName);
+
+var cobaBoolean = true;
+console.log(cobaBoolean);
+
+var job = "Engineer";
+console.log(job);
+
+var bio = "Nama saya "+ fullName +". Umur saya "+ age+". Pekerjaan saya "+ job;
+console.log(bio);
